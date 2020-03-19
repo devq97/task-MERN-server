b294236cf7938624c3244a876925218ea49eb2df
DavidVargas <david.vargas@decameron.com>
2020-03-19 09:40:40 -0500

    Se inicia el servidor Express Se hace la conexión a la base de datos Se crea la base de datos en Mongo Atlas

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('desde index');
+const express = require('express');
+const connection = require('./config/db');
+
+// Create the server
+const app = express();
+
+// Connect to DB
+connection();
+
+// App Port
+const PORT = process.env.PORT || 4000;
+
+// Define Main Page
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+// Start App
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+})
