dba0ea95cf813d7bc1c0483a546334568fe68713
DavidVargas <david.vargas@decameron.com>
2020-03-19 13:11:39 -0500

    Se hace el CRUD  de proyectos

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -26,3 +26,82 @@ exports.createProject = async (req, res) => {
     res.status(500).send('Error');
   }
 };
+
+// Get projects for user
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ creator: req.user.id }).sort({ creator: -1 });
+    res.status(200).json({ projects });
+
+  } catch (error) {
+    res.status(500).send('Error');
+  }
+};
+
+// Update project
+exports.updateProject = async (req, res) => {
+
+  // Validate Errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  // Extract project info
+  const { name } = req.body;
+  const newProject = {};
+  if (name) {
+    newProject.name = name;
+  }
+
+  try {
+
+    // Check ID
+    let project = await Project.findById(req.params.id);
+
+    // Check if project exists
+    if (!project) {
+      return res.status(404).json({ msg: 'Project not found' });
+    }
+
+    // Verify creator
+    if (project.creator.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Unauthorized' });
+    }
+
+    // Update project
+    project = await Project.findByIdAndUpdate({ _id: req.params.id }, { $set: newProject }, { new: true });
+    res.json({project});
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error');
+  }
+};
+
+// Delete project
+exports.deleteProject = async (req, res) => {
+
+  try {
+    // Check ID
+    let project = await Project.findById(req.params.id);
+
+    // Check if project exists
+    if (!project) {
+      return res.status(404).json({ msg: 'Project not found' });
+    }
+
+    // Verify creator
+    if (project.creator.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Unauthorized' });
+    }
+
+    // Delete project
+    await Project.findOneAndRemove({ _id: req.params.id });
+    res.status(200).json({ msg: 'Project deleted' });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error');
+  }
+};
--- routes/projects.js
+++ routes/projects.js
@@ -15,9 +15,25 @@ router.post('/',
   projectController.createProject
 );
 
+// Get all projects
 router.get('/',
   auth,
-  projectController.createProject
+  projectController.getProjects
+);
+
+// Update project
+router.put('/:id',
+  auth,
+  [
+    check('name', 'Project name is required').not().isEmpty()
+  ],
+  projectController.updateProject
+);
+
+// Delete project
+router.delete('/:id',
+  auth,
+  projectController.deleteProject
 );
 
 module.exports = router;
