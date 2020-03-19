32a2357eb7df09eee239ae9f626e56d6c2bf7be1
DavidVargas <david.vargas@decameron.com>
2020-03-19 11:01:44 -0500

    Se crea la inserción de usuarios Se hashea las password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,37 @@
-exports.createUser = (req, res) => {
-  console.log(req.body)
+const User = require('../models/User');
+const bcryptjs = require('bcryptjs');
+
+exports.createUser = async (req, res) => {
+
+  // Extract email and password
+  const { email, password } = req.body;
+
+  try {
+    // Evaluate the unique Email
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(409).json({
+        msg: 'The User already exists'
+      });
+    }
+
+    // Create new User
+    user = new User(req.body)
+
+    // Hash to the password
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(password, salt);
+
+    // Save new User
+    await user.save();
+
+    // Confirmation Message
+    return res.status(200).json({
+      msg: 'User created'
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error Saving User');
+  }
 };
